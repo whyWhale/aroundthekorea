6ab77bc9f385c81a28b7ce97b7513a6f2ba1e83c
whyWhale <soccer_by@naver.com>
2024-07-13 14:25:35 +0900

    setting: backend init

---
+++ backend/src/main/java/com/travel/aroundthekorea/AroundthekoreaApplication.java
@@ -0,0 +1,13 @@
+package com.travel.aroundthekorea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundthekoreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AroundthekoreaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/travel/aroundthekorea/AroundthekoreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.aroundthekorea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AroundthekoreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
