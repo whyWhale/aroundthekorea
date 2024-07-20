ac20381adbb809055f88cedc3e9d3afb269744a5
whyWhale <soccer_by@naver.com>
2024-07-20 16:15:52 +0900

    setting: 환경셋팅
    - 환경 분리
    - axios, vuetify 모듈 추가
    - eslint,prettier 설정

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -2,13 +2,47 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+	root: true,
+	extends: [
+		'plugin:vue/vue3-essential',
+		'eslint:recommended',
+		'@vue/eslint-config-prettier/skip-formatting',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	plugins: ['prettier'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				singleQuote: true,
+				semi: false,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				printWidth: 80,
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+				endOfLine: 'auto',
+			},
+		],
+		semi: ['error', 'never'],
+		quotes: ['error', 'single'],
+		'vue/html-self-closing': [
+			'error',
+			{
+				html: {
+					void: 'always',
+					normal: 'never',
+					component: 'always',
+				},
+				svg: 'always',
+				math: 'always',
+			},
+		],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+	},
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
 }
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,18 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd())
+	const DEFAULT_PORT = 3000
+
+	return {
+		plugins: [vue(), vueDevTools()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		server: {
+			port: env.VITE_PORT ? env.VITE_PORT : DEFAULT_PORT,
+		},
+	}
 })
