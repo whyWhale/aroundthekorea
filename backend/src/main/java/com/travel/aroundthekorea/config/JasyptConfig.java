41a139933d010a086040d2f1197dff0727794725
whyWhale <soccer_by@naver.com>
2024-07-21 23:49:56 +0900

    setting: 민감정보 암호화

---
+++ backend/src/main/java/com/travel/aroundthekorea/config/JasyptConfig.java
@@ -0,0 +1,37 @@
+package com.travel.aroundthekorea.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@Configuration
+@EnableEncryptableProperties
+@Profile({"local", "prod"})
+public class JasyptConfig {
+
+	@Value("${jasypt.key}")
+	private String password;
+
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(password);
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+
+		return encryptor;
+	}
+}
--- backend/src/main/java/com/travel/aroundthekorea/controller/HelloController.java
+++ backend/src/main/java/com/travel/aroundthekorea/controller/HelloController.java
@@ -1,15 +1,25 @@
 package com.travel.aroundthekorea.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping
 public class HelloController {
 
+	@Value("${data.service-key}")
+	private String serviceKey;
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello world";
+	}
+
+	@GetMapping("/test")
+	public String testgo() {
+		return serviceKey;
 	}
 }
